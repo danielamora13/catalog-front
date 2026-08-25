867c5e57671ec87b9e148d42ca69ae2f85886906
Daniela <danielamora13@hotmail.com>
2026-08-25 12:44:24 +0200

    fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'home/escape-room-detail/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'home/escape-room-edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
